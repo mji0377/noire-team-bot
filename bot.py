214e9e8363f33b6724a88a1e8f7b5b364ead541c
mji0377 <moonji8222@gmail.com>
2026-05-30 21:48:31 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("봇 연결 성공!")
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
